076dddedbc672a527f36b2964e6a7462431add38
thanus <thanus@users.noreply.github.com>
2018-12-09 19:00:14 +0100

    Get statistics by account name

--- statistics-service/src/main/kotlin/com/piggymetrics/kt/statistics/controllers/StatisticsController.kt
+++ statistics-service/src/main/kotlin/com/piggymetrics/kt/statistics/controllers/StatisticsController.kt
@@ -3,14 +3,16 @@ package com.piggymetrics.kt.statistics.controllers
 import com.piggymetrics.kt.statistics.domain.Account
 import com.piggymetrics.kt.statistics.domain.timeseries.DataPoint
 import com.piggymetrics.kt.statistics.services.StatisticsService
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
+import javax.validation.constraints.NotBlank
 
 @RestController
 class StatisticsController(private val statisticsService: StatisticsService) {
+
+    @GetMapping("/{accountName}")
+    fun getStatisticsByAccountName(@NotBlank @PathVariable accountName: String): List<DataPoint> =
+            statisticsService.findByAccountName(accountName)
 
     @PutMapping("/{accountName}")
     fun saveAccountStatistics(@PathVariable accountName: String, @Valid @RequestBody account: Account) {
--- statistics-service/src/main/kotlin/com/piggymetrics/kt/statistics/services/StatisticsService.kt
+++ statistics-service/src/main/kotlin/com/piggymetrics/kt/statistics/services/StatisticsService.kt
@@ -4,5 +4,6 @@ import com.piggymetrics.kt.statistics.domain.Account
 import com.piggymetrics.kt.statistics.domain.timeseries.DataPoint
 
 interface StatisticsService {
+    fun findByAccountName(accountName: String): List<DataPoint>
     fun save(accountName: String, account: Account): DataPoint
 }
